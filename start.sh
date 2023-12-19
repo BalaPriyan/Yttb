6d991d73921998d8c07b7db64e8dda8113eb074e
Balapriyan B <131746104+BalaPriyan@users.noreply.github.com>
2023-12-20 00:06:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,1 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/BalaPriyan/Yttb /VJ-V2
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO"
-  git clone $UPSTREAM_REPO /UHD_Tamil
-fi
-
-cd /UHD_Tamil
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
 python3 bot.py
